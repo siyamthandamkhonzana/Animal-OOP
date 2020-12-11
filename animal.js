3a09bf3c2c02b155995080816c55a6828299aadf
unknown <smkhonzana@gmail.com>
2020-12-11 11:23:52 +0200

    changes made to sr file

--- animal.js
+++ animal.js
@@ -1,66 +1,44 @@
-class Animal{
-    constructor(name){
-        this.name =name ;
-    }
-sound(){
-    console.log(`sound...`)
-}
-eat(){
-    console.log(`${this.name} eat`)
-}
-}
-class Dog extends Animal{
-    constructor(name){
-        super(name);
-        this.name = "Rax"
-    }
-    sounds(){
-        console.log(`Dog barks`)
-    }
-}
-class Cat extends Animal{
-    constructor(name){
-        super(name);
-        this.name = "Stormy"
-    }
-    sounds(){
-        console.log(`Cat meows`)
-    }
-}
-var adoptedPet = [] ;
-class Home {
-  adoptPet(animal){
-  adoptedPet.push(animal)
+class Animal {
+  constructor(name) {
+    this.name = name;
   }
-makeAllSounds(){
-    for(var i = 1; i < adoptedPet.length; i++){
-        adoptedPet[i].sounds();
+  sound() {
+    console.log(`sound...`);
+  }
+  eat() {
+    console.log(`${this.name} eat`);
+  }
+}
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+    this.name = "Rax";
+  }
+  sounds() {
+    console.log(`Dog barks`);
+  }
+}
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+    this.name = "Stormy";
+  }
+  sounds() {
+    console.log(`Cat meows`);
+  }
+}
+class Home {
+  constructor() {
+    this.adoptedPets = [];
+  }
+  adoptPet(animal) {
+    this.adoptedPets.push(animal);
+  }
+  makeAllSounds() {
+    for (var i = 1; i < this.adoptedPets.length; i++) {
+      this.adoptedPets[i].sounds();
     }
+  }
 }
-}
-
-var dog = new Dog();
-dog.eat();
-dog.sounds();
-
-var cat = new Cat();
-cat.eat();
-cat.sounds();
-
-var home = new Home();
-var dog1 = new Dog();
-var dog2 = new Dog();
-var cat = new Cat();
-
-home.makeAllSounds();
-home.adoptPet(dog1);
-home.makeAllSounds();
-
-home.adoptPet(cat);
-home.makeAllSounds();
-
-home.adoptPet(dog2);
-home.makeAllSounds();
-
 
 
